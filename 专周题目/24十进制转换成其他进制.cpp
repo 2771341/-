c7aab0e874378cd96d796fdf79b7e64ab76c77e0
2771341 <154664105+2771341@users.noreply.github.com>
2024-01-03 22:42:01 +0800

    Update 24十进制转换成其他进制.cpp

--- 专周题目/24十进制转换成其他进制.cpp
+++ 专周题目/24十进制转换成其他进制.cpp
@@ -1,27 +1,72 @@
 #include <stdio.h>
-void decimalToBinary(int n) {
-    printf("Ê®½øÖÆ %d ×ª»»Îª¶þ½øÖÆ: ", n);
-    for (int i = 31; i >= 0; i--) {
-        int bit = (n >> i) & 1;
-        printf("%d", bit);
+#define M 100
+int s[M];
+int t=-1;
+void p(int i) {
+    if (t==M - 1) 
+	{
+        printf("æ»¡\n");
+        return;
+    }
+    s[++t]=i;
+}
+int pop() {
+    if (t==-1) {
+        printf("ç©º\n");
+        return -1;
+    }
+    return s[t--];
+}
+void c(int n, int b) {
+    int o=n;
+    if (n==0) {
+        printf("%d è½¬", n);
+        if (b==2) {
+            printf("2: 0\n");
+        } else if (b==8) {
+            printf("8: 0\n");
+        } else if (b==16) {
+            printf("16: 0\n");
+        }
+        return;
+    }
+    while (n>0) 
+	{
+        p(n%b);
+        n/=b;
+    }
+    printf("%d è½¬", o);
+    if (b==2) {
+        printf("2: ");
+    } else if (b==8) 
+	{
+        printf("8: ");
+    } else if (b==16) 
+	{
+        printf("16: ");
+    }
+    while (t!=-1) 
+	{
+        int d=pop();
+        if (b==16 && d>=10) 
+		{
+            printf("%c", 'A'+d-10);
+        } else 
+		{
+            printf("%d", d);
+        }
     }
     printf("\n");
 }
-void decimalToOctal(int n) {
-    printf("Ê®½øÖÆ %d ×ª»»Îª°Ë½øÖÆ: %o\n", n, n);
-}
-void decimalToHexadecimal(int n) {
-    printf("Ê®½øÖÆ %d ×ª»»ÎªÊ®Áù½øÖÆ: %X\n", n, n);
-}
-int main() {
-    int num1 = 10;
-    int num2 = 31;
-    decimalToBinary(num1);
-    decimalToOctal(num1);
-    decimalToHexadecimal(num1);
-    decimalToBinary(num2);
-    decimalToOctal(num2);
-    decimalToHexadecimal(num2);
+int main() 
+{
+    int n1=10;
+    int n2=31;
+    c(n1,2);
+    c(n1,8);
+    c(n1,16);
+    c(n2,2);
+    c(n2,8);
+    c(n2,16);
     return 0;
 }
-
